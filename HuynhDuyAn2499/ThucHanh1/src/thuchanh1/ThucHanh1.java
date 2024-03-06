1c57153f0338ebd7b336a4b35a9725585ecb1304
huynhduyan15 <161592147+huynhduyan15@users.noreply.github.com>
2024-03-06 19:35:01 +0700

    nop

---
+++ HuynhDuyAn2499/ThucHanh1/src/thuchanh1/ThucHanh1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thuchanh1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author
+ */
+public class ThucHanh1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormChinh frm = new FormChinh();
+        frm.setVisible(true);
+        frm.setSize(600,400);
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
